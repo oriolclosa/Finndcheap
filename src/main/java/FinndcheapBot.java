999c557ab4986bdb8eb757fe83a84530dbcd83a9
oriol20 <oriol.closa@est.fib.upc.edu>
2017-11-25 07:34:47 +0100

    Nou

--- src/main/java/FinndcheapBot.java
+++ src/main/java/FinndcheapBot.java
@@ -2,8 +2,33 @@ import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import com.mongodb.MongoClient; import com.mongodb.MongoClientURI; import com.mongodb.client.MongoCollection; import com.mongodb.client.MongoDatabase; import org.bson.Document; import org.json.JSONObject;
 
 public class FinndcheapBot extends TelegramLongPollingBot {
+    private String check(String first_name, String last_name, int user_id, String username) {
+        MongoClientURI connectionString = new MongoClientURI("mongodb://159.89.13.211:27017");
+        MongoClient mongoClient = new MongoClient(connectionString);
+        MongoDatabase database = mongoClient.getDatabase("db_name");
+        MongoCollection<Document> collection = database.getCollection("users");
+        long found = collection.count(Document.parse("{id : " + Integer.toString(user_id) + "}"));
+        if (found == 0) {
+            Document doc = new Document("first_name", first_name)
+                    .append("last_name", last_name)
+                    .append("id", user_id)
+                    .append("username", username);
+            collection.insertOne(doc);
+            mongoClient.close();
+            System.out.println("User not exists in database. Written.");
+            return "no_exists";
+        } else {
+            System.out.println("User exists in database.");
+            mongoClient.close();
+            return "exists";
+        }
+
+
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
 
